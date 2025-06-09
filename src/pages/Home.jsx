76c0df75e8d9861dc0490c0304d630c5dda632e3
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-09 16:43:21 +0000

    I imported the Sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
 
 
 function Home (){
     return(
         <>
+        <Sidebar />
         <Navbar />
         </>
     );
