179e3188c157ae4f33de1d638fe21aa56814d534
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-09 15:00:37 +0000

    started working on the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+
 function Home (){
     return(
-        <div>
-            <h1 className="text-4xl text-purple-600">This is our Home Page</h1>
-        </div>
-
+        <>
+        <Navbar />
+        </>
     );
 }
 export default Home
