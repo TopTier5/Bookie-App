bdf748bf5d2685cfa4d60c64ccc07b91547ac770
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-09 20:55:09 +0000

    sidebar committed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
+import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import Sidebar from "../components/Sidebar";
 
 
 function Home (){
     return(
         <>
+        <div>
         <Sidebar />
+
+        <div>
         <Navbar />
-        </>
+        <section className=" flex min-h-screen flex-1">
+            <div className="grid grid-cols-4 gap-5 border-b-grey">
+                <h1>Total books <br /> 6</h1> 
+                <h3>Total collection</h3>
+                
+            </div>
+
+        </section>
+        </div>
+        </div>
+         </>
     );
 }
 export default Home
