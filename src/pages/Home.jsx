66e64842a67268f33d0a618155b547aa4dba7379
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-09 23:06:06 +0000

    worked on the grid elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,28 +3,47 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 
-function Home (){
-    return(
+function Home() {
+    return (
         <>
-        <div className="flex h-screen">
-            {/* Sidebar on the left */}
-            <aside className="w-64 bg-[#AD856F]">
-              <Sidebar />
-            </aside>
+            <div className="flex h-screen">
+                {/* Sidebar on the left */}
+                <aside className="w-64 bg-[#AD856F]">
+                    <Sidebar />
+                </aside>
 
-            {/* Main component on the right */}
-            <div className="flex flex-col flex-1">
-            {/* Navbar at the top */}
-            <Navbar />
+                {/* Main component on the right */}
+                <div className="flex flex-col flex-1">
+                    {/* Navbar at the top */}
+                    <Navbar />
 
-            {/* Page content */}
-            <main className="p-6 bg-white flex-1 overflow-auto">
-            </main>
+                    {/* Page content */}
+                    <main className="p-6 bg-white flex-1 overflow-auto">
+                        <div className="grid grid-cols-4 gap-5">
+                            <div className="border font-sans p-4  border-grey rounded shadow text-black">
+                                <h1 className="text-sm font-bold">Total Books</h1>
+                                <p className="text-2xl font-bold">6</p>
+                                <h5 className="text-xs">Total collection</h5>
+                            </div>
+                            <div className="border font-sans p-4 border-grey rounded shadow text-black">
+                                <h1 className="text-sm font-bold ">Available</h1>
+                                <p className="text-2xl text-emerald-800 font-bold">24</p>
+                            </div>
+                            <div className="border font-sans p-4 rounded shadow text-black">
+                                <h1 className="text-sm font-bold">Checked out</h1>
+                                <p className="text-2xl">10</p>
+                            </div>
+                            <div className="border font-sans p-4 rounded shadow text-black">
+                                <h1 className="text-lg font-bold">Available</h1>
+                                <p className="text-2xl">14</p>
+                            </div>
+                        </div>
+                    </main>
+                </div>
             </div>
-            </div>
 
 
-         </>
+        </>
     );
 }
 export default Home
