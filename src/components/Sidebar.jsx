db80452be9e0f6aebc219ce0628ce738567c6785
James Junior Baah <jjbaah068@gmail.com>
2025-06-10 10:37:13 -0700

    redirects update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ function Sidebar() {
     return ( 
         <nav className="bg-white w-64 h-screen border-r border-gray-400">
             <div className="flex items-center gap-3 px-4 py-4">
-                    <div className="ml-5 px-3 bg-[#33384C]">
+                    <div className="ml-5 px-3 bg-[#33384C] border-b">
                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-icon lucide-book text-white">
                          <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20" /></svg>
                          <span>
