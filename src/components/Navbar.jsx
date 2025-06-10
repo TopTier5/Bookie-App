74c68b1cc3b7d9209d45a0c7917cc78dab41c6d6
James Junior Baah <jjbaah068@gmail.com>
2025-06-09 20:40:47 -0700

    sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 function Navbar() {
     return (
-        <nav className="bg-[#AD856F] py-4 flex items-center">
-              
+        <nav className="bg-[#6B7280] px-6 py-4 shadow-sm w-full">
+            <div className="">
+                <h2 className="text-xl font-semibold text-white">Library Dashboard</h2>
+                <p className="text-sm text-white">Manage your collection</p>
+            </div>
+
+              <div className="flex items-center gap-4">
+                <input type="text" placeholder="search books..." className="border border-gray-300 rounded-md px-3 py-1 text-sm" />
+              </div>
         </nav>
     );
 }
