861ddfcf59979bd953a289f7259b9ba4b1552bf5
James Junior Baah <jjbaah068@gmail.com>
2025-06-09 21:49:02 -0700

    navbar and sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 function Navbar() {
     return (
-        <nav className="bg-[#6B7280] px-6 py-4 shadow-sm w-full">
-            <div className="">
-                <h2 className="text-xl font-semibold text-white">Library Dashboard</h2>
-                <p className="text-sm text-white">Manage your collection</p>
+        <nav className="bg-white px-6 py-4 shadow-sm w-full">
+            <div className="flex items-center gap-4">
+                <h2 className="text-lg font-light text-black">Library Dashboard <p className="text-sm text-gray-400">Manage your book collection</p></h2>
+                
+                <div className="ml-[250px]">
+                <input type="text" placeholder="Search books..." className="border border-gray-300 rounded-md px-10 py-2 text-sm" />
+              </div>
+
+              <div className="ml-10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bell-icon lucide-bell"><path d="M10.268 21a2 2 0 0 0 3.464 0"/><path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326"/></svg>
+              </div>
+
+              <div className="ml-12">
+                <button className="bg-[#2563EB] hover:bg-[#1D4ED8] text-white px-5 py-2 rounded-md text-sm font-medium">+ Add Book</button>
+              </div>
+
             </div>
 
-              <div className="flex items-center gap-4">
-                <input type="text" placeholder="search books..." className="border border-gray-300 rounded-md px-3 py-1 text-sm" />
-              </div>
+              
+              
         </nav>
     );
 }
