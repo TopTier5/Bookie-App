a48cdb77a20dddbe9c210ec1eb5dbe54dd1d0d1b
James Junior Baah <jjbaah068@gmail.com>
2025-06-10 13:56:41 -0700

    sidebar updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,53 +1,38 @@
+import AppLogo from "../assets/images/pic2.png"
+import { Link } from "react-router";
+
 function Sidebar() {
-    return ( 
+    return (
         <nav className="bg-white w-64 h-screen border-r border-gray-400">
-            <div className="flex items-center gap-3 px-4 py-4">
-                    <div className="ml-5 px-3 bg-[#33384C] border-b">
-                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-icon lucide-book text-white">
-                         <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20" /></svg>
-                         <span>
-                            <h1 className="text-lg font-semibold">TopTierLMS</h1>
-                           <p className="text-xs py-1">v1.0</p></span>
-
-                         
-                    </div>
-                   
+            <div className="flex items-center gap-3 px-4 py-4 border-b border-gray-400">
+                <div className="bg-[#1D4ED8]">
+                    <Link to="/">
+                        <img src={AppLogo} alt="Logo" className="h-30 w-30 object-contain " />
+                    </Link>
                 </div>
-                
-            
+            </div>
 
-                <ul className="mt-3 space-y-3 px-7">
-                    <li className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-plus-icon lucide-house-plus"><path d="M13.22 2.416a2 2 0 0 0-2.511.057l-7 5.999A2 2 0 0 0 3 10v9a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7.354"/><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/><path d="M15 6h6"/><path d="M18 3v6"/></svg>
-                      <span>Home</span>  
-                    </li>
-                    <li className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                        <span>Add Book</span>  
-                    </li>
+            <ul className="mt-3 space-y-3 px-7">
+                <li className="flex items-center gap-3 mb-5">
+                    <Link to="/" className="flex items-center gap-3 py-2 px-6 text-gray-700 rounded-lg hover:border-2 hover:border-blue-200 hover:text-bue-700 transition-all duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-house-plus-icon lucide-house-plus"><path d="M13.22 2.416a2 2 0 0 0-2.511.057l-7 5.999A2 2 0 0 0 3 10v9a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7.354" /><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M15 6h6" /><path d="M18 3v6" /></svg>
+                        <span>Dashboard</span></Link>
 
-                    <li className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen-icon lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
-                      <span>Edit Form</span>  
-                    </li>
+                </li>
 
-                    <li className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-chart-gantt-icon lucide-square-chart-gantt"><rect width="18" height="18" x="3" y="3" rx="2"/><path d="M9 8h7"/><path d="M8 12h6"/><path d="M11 16h5"/></svg>
-                      <span>View Books</span>  
-                    </li>
-                    <li className="flex items-center gap-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-icon lucide-book-open"><path d="M12 7v14"/><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"/></svg>
-                        <span>Book Details</span>  
-                    </li>
-                </ul>
-               
-    
+                <li className="flex items-center gap-3 mb-5">
+                    <Link to="/addbook" className="flex items-center gap-3 py-2 px-6 text-gray-700 rounded-lg hover:border-2 hover:border-blue-200 hover:text-bue-700 transition-all duration-200"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14" /><path d="M12 5v14" /></svg>
+                        <span>Add Book</span></Link></li>
 
-
-
+                <li className="flex items-center gap-3 mb-5">
+                    <Link to="/displaypages" className="flex items-center gap-3 py-2 px-6 text-gray-700 rounded-lg hover:border-2 hover:border-blue-200 hover:text-bue-700 transition-all duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-chart-gantt-icon lucide-square-chart-gantt"><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M9 8h7" /><path d="M8 12h6" /><path d="M11 16h5" /></svg>
+                        <span>View Books</span>
+                    </Link>
+                </li>
+            </ul>
 
             <div>
-
             </div>
 
         </nav>
