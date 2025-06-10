750d25e3c15542534e27435d8dc4ff2c0df049c6
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-10 14:46:26 +0000

    I removed the footer from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,5 @@
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-
 
 function Home() {
     return (
@@ -12,16 +10,16 @@ function Home() {
                 </aside>
                 <div className="flex flex-col flex-1">
                     <Navbar />
-                    <main className="p-6 bg-white flex-1 overflow-auto">
+                    <hero className="p-6 bg-white flex-1 overflow-auto">
                         <div className="grid grid-cols-4 gap-5">
                             <div className="border font-sans p-4  border-grey rounded shadow text-black">
                                 <h1 className="text-sm font-bold">Total Books <span className="text-2xl">📘</span></h1>
-                                <p className="text-2xl font-bold">6</p>
+                                <p className="text-2xl font-bold">30</p>
                                 <h5 className="text-xs">Total collection</h5>
                             </div>
                             <div className="border font-sans p-4 border-grey rounded shadow text-black">
                                 <h1 className="text-sm font-bold">Available <span className="text-2xl">✅</span></h1>
-                                <p className="text-2xl text-emerald-800 font-bold">24</p>
+                                <p className="text-2xl text-emerald-800 font-bold">14</p>
                                 <h5 className="text-xs">Books available</h5>
                             </div>
                             <div className="border font-sans p-4 rounded shadow text-black">
@@ -31,11 +29,14 @@ function Home() {
                             </div>
                             <div className="border font-sans p-4 rounded shadow text-black">
                                 <h1 className="text-lg font-bold">Reserved <span className="text-2xl" >🪪</span></h1>
-                                <p className="text-2xl text-violet-900 font-bold">14</p>
+                                <p className="text-2xl text-violet-900 font-bold">6</p>
                                 <h5 className="text-xs">Books reserved</h5>
                             </div>
                         </div>
-                    </main>
+                        <div className=" mt-7 bg-blue-100 p-4 rounded-lg shadow-md mb-6"><h2 className="text-xl font-semibold text-blue-900">📚 Trending Reads</h2>
+                        </div>
+
+                    </hero>
                 </div>
             </div>
 
