d9d14a5338849651fba2e68616e7509cf9171c62
vonette-ab <vonwrkspc@gmail.com>
2025-06-11 16:02:14 +0000

    editform 1st edit

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { apiClient } from "../api/client";
 
+
+
 export default function AddBook() {
     const postBook = (event) => {
         event.preventDefault();
@@ -26,8 +28,8 @@ export default function AddBook() {
     return (
         <>
             <Navbar />
-            <section className="bg-[#FFF3DF] ">
-                <div className="w-[60%] mx-auto bg-[#FFFDF8]  border border-[#FDE68A] rounded-2xl shadow-lg p-8">
+            <section className="bg-[#FFF3DF] py-8 ">
+                <div className="w-[60%] mx-auto bg-[#FFFDF8]  border border-[#FDE68A] rounded-2xl shadow-lg p-8 ">
                     <form onSubmit={postBook} className="flex flex-col space-y-6 text-center">
                         <h1 className="text-black text-2xl font-bold "> 📖 Add New Book</h1>
                         <h3 className="text-black">Fill in the details below to add a new book to your library.</h3>
@@ -63,8 +65,8 @@ export default function AddBook() {
                                         className="w-full border border-blue-200 rounded px-4 py-2"
                                     />
                                 </div>
-                            </div>    
-                                
+                            </div>
+
                             <div className="w-1/2">
                                 <div>
                                     <label className="block mb-2 font-bold text-black">Publication Year</label>
@@ -74,11 +76,11 @@ export default function AddBook() {
                                         className="w-full border border-blue-200 rounded px-4 py-2"
                                     />
                                 </div>
-                            </div>    
+                            </div>
                         </div>
 
-                        
-                         <div>
+
+                        <div>
                             <label className="block mb-2 font-bold text-black">Cover Image URL</label>
                             <input
                                 type="text"
@@ -87,18 +89,20 @@ export default function AddBook() {
                             />
                         </div>
 
-                        
+
+                       
                         <div className="flex justify-end">
-                            <button
-                                type="submit"
-                                className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-800 transition-colors"
-                            >
-                                Add Book
-                            </button>
-                        </div>
+                                <button
+                                    type="submit"
+                                    className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-800 transition-colors"
+                                >
+                                    Add Book
+                                </button>
+                            </div>
                     </form>
                 </div>
             </section>
         </>
     );
 }
+
--- src/pages/EditForm.jsx
+++ src/pages/EditForm.jsx
@@ -1,14 +1,76 @@
 import Navbar from "../components/Navbar";
-import React, { useState } from "react";
 
 
-export default function EditForm() {
-    
+function EditForm() {
+
     return (
         <>
             <Navbar />
-           
+            
+            <section className="bg-[#FFF3DF] py-8 ">
+                <div className="w-[60%] mx-auto my-[5%] bg-[#FFFDF8]  border border-[#FDE68A] rounded-2xl shadow-lg p-8">
+                    <form className="flex flex-col space-y-6 text-center">
+                        <h1 className="text-black text-2xl font-bold self-center"> 📔 Edit Book</h1>
+
+                        <div className="grid grid-cols-2 gap-4 justify-between space-x-4">
+                            <div className="w-20%">
+                                <label className="block mb-2 font-bold text-black">Title*</label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    className="w-full border border-blue-200 rounded px-4 py-2"
+                                />
+                            </div>
+
+                            <div className="w-20%">
+                                <label className="block mb-2 font-bold text-black">Author*</label>
+                                <input
+                                    type="text"
+                                    name="author"
+                                    className="w-full border border-blue-200 rounded px-4 py-2"
+                                />
+                            </div>
+                        
+
+
+                        
+                            <div className="w-20%">
+                                <label className="block mb-2 font-bold text-black">Publication Year</label>
+                                <input
+                                    type="date"
+                                    name="publicationYear"
+                                    className="w-full border border-blue-200 rounded px-4 py-2"
+                                />
+                            </div>
+
+
+
+                            <div className="w-20%">
+                                <label className="block mb-2 font-bold text-black">Cateogry</label>
+                                <input
+                                    type="text"
+                                    name="category"
+                                    className="w-full border border-blue-200 rounded px-4 py-2"
+                                />
+                            </div>
+                        </div>
+
+
+                        <div className="flex justify-end">
+                            <button
+                                type="submit"
+                                className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-800 transition-colors"
+                            >
+                                Edit Book
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
         </>
     )
 }
 
+
+export default EditForm; 
