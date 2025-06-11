ecd8050433ccabd0ddb33ae375e138391f5d1fa8
Miss_Christie <christianakobea@gmail.com>
2025-06-11 14:20:07 +0000

    single page commit

--- src/pages/DisplayPages.jsx
+++ src/pages/DisplayPages.jsx
@@ -4,9 +4,15 @@ import BookCard from "../components/BookCard";
 function DisplayPages() {
     return (
         <>
-            <h1 className="text-4xl text-purple-600">This is our Display Pages</h1>
+            <div className="border rounded-lg border-gray-50 bg-gray-50 w-[35%] h-10 mt-5 flex justify-around items-center ml-5">
+                <span className="hover:bg-white px-2 py-2 rounded text-sm font-medium bg-white cursor-pointer ml-0">All Pages</span>
+                <span className="hover:bg-white px-2 py-2 rounded  text-sm font-medium cursor-pointer">Available</span>
+                <span className="hover:bg-white px-2 py-2 rounded  text-sm font-medium cursor-pointer">Checked Out</span>
+                <span className="hover:bg-white px-2 py-2 rounded  text-sm font-medium cursor-pointer">Reserved</span>
+            </div>
+
             <div className="grid grid-cols-4 gap-5">
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16].map(n => <BookCard key={n} />)}
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(n => <BookCard key={n} />)}
 
 
             </div>
