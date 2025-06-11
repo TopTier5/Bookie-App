5c12945f42f985b1fc2cb73fcf9b1960c824725a
James Junior Baah <jjbaah068@gmail.com>
2025-06-10 20:09:53 -0700

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router";
 
 function Sidebar() {
     return (
-        <nav className="bg-white w-64 h-screen border-r-1 border-gray-400">
+        <nav className="bg-white w-64 h-screen border-r border-gray-300">
            
-            <div className="flex items-center space-x-3 p-4 border-b-1 border-gray-400">
+            <div className="flex items-center space-x-3 p-4 border-b border-gray-300">
                 <div className="bg-[#1D4ED8] w-8 h-8 flex items-center justify-center rounded-md">
                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-icon lucide-book"><path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/></svg>
                 </div>
