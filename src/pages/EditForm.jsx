ee6244a9a26df3c68d16b9474f89cc16d514bad6
vonette-ab <vonwrkspc@gmail.com>
2025-06-13 15:42:45 +0000

    display pages

--- src/pages/EditForm.jsx
+++ src/pages/EditForm.jsx
@@ -46,7 +46,7 @@ export default function EditForm() {
         <>
             <div className="flex items-center justify-between mb-6 bg-white w-full ">
                 <div className="flex flex-col gap-4 p-4 w-full md:flex-row md:items-center md:justify-between">
-                    <Link to="/singlepage" className="flex items-center text-gray-600 hover:text-blue-600 ">
+                    <Link to="/displaypages" className="flex items-center text-gray-600 hover:text-blue-600 ">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2"
                             viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
