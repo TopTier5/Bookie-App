c3cc9e6bc9480b167113f1d86f8008f375773778
elizabethharrison-akortiah <elizabethtemi1@gmail.com>
2025-06-13 10:36:31 +0000

    I added a background colour

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,49 +21,51 @@ function Home() {
     useEffect(getBooks, []);
 
 
-        return (
-            <>
-                <div className="flex h-screen">
-                    <aside className="w-64 bg-[#AD856F] hidden md:block">
-                        <Sidebar />
-                    </aside>
-                    <div className="flex flex-col flex-1">
-                        <Navbar />
-                        <main className="p-6 bg-[#F9FAFB] flex-1 overflow-auto">
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5">
-                                <div className="border bg-white font-sans p-4  border-grey rounded shadow text-black">
-                                    <h1 className="text-sm font-bold">Total Books <span className="text-2xl">📘</span></h1>
-                                    <p className="text-2xl font-bold">30</p>
-                                    <h5 className="text-xs">Total collection</h5>
-                                </div>
-                                <div className="border bg-white font-sans p-4 border-grey rounded shadow text-black">
-                                    <h1 className="text-sm font-bold">Available <span className="text-2xl">✅</span></h1>
-                                    <p className="text-2xl text-emerald-800 font-bold">14</p>
-                                    <h5 className="text-xs">Books available</h5>
-                                </div>
-                                <div className="border bg-white font-sans p-4 rounded shadow text-black">
-                                    <h1 className="text-sm font-bold">Checked out<span className="text-2xl" >📊</span></h1>
-                                    <p className="text-2xl  text-orange-600 font-bold">10</p>
-                                    <h5 className="text-xs">Books checked out</h5>
-                                </div>
-                                <div className="border bg-white font-sans p-4 rounded shadow text-black">
-                                    <h1 className="text-lg font-bold">Reserved <span className="text-2xl" >🪪</span></h1>
-                                    <p className="text-2xl text-violet-900 font-bold">6</p>
-                                    <h5 className="text-xs">Books reserved</h5>
-                                </div>
+    return (
+        <>
+            <div className="flex h-screen">
+                <aside className="w-64 bg-[#AD856F] hidden md:block">
+                    <Sidebar />
+                </aside>
+                <div className="flex flex-col flex-1">
+                    <Navbar />
+                    <main className="p-6 bg-[#404c6d] flex-1 overflow-auto">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                            <div className="border bg-white font-sans p-4  border-pink-400 rounded shadow text-black">
+                                <h1 className="text-sm font-bold">Total Books <span className="text-2xl">📘</span></h1>
+                                <p className="text-2xl font-bold">30</p>
+                                <h5 className="text-xs">Total collection</h5>
                             </div>
-                            <div className=" mt-7 bg-blue-200 p-4 rounded shadow-md mb-6 trending-bounce"><h2 className="text-xl font-semibold text-blue-900">🔥 Trending Reads</h2>
+                            <div className="border bg-white font-sans p-4 border-pink-400 rounded shadow text-black">
+                                <h1 className="text-sm font-bold">Available <span className="text-2xl">✅</span></h1>
+                                <p className="text-2xl text-emerald-800 font-bold">14</p>
+                                <h5 className="text-xs">Books available</h5>
                             </div>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
-                                {books.map(book => <BookCard key={book.id} book={book} />)}
+                            <div className="border bg-white font-sans p-4 border-pink-400 rounded shadow text-black">
+                                <h1 className="text-sm font-bold">Checked out<span className="text-2xl" >📊</span></h1>
+                                <p className="text-2xl  text-orange-600 font-bold">10</p>
+                                <h5 className="text-xs">Books checked out</h5>
                             </div>
+                            <div className="border bg-white font-sans border-pink-400 p-4 rounded shadow text-black">
+                                <h1 className="text-lg font-bold">Reserved <span className="text-2xl" >🪪</span></h1>
+                                <p className="text-2xl text-violet-900 font-bold">6</p>
+                                <h5 className="text-xs">Books reserved</h5>
+                            </div>
+                        </div>
+                        <div className=" mt-7 bg-blue-200 p-4 rounded shadow-md mb-6 trending-bounce"><h2 className="text-xl font-semibold text-blue-900">🔥 Trending Reads</h2>
+                        </div>
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-5">
+                            {books.slice(0, 6).map((book) => (
+                                <BookCard key={book.id} book={book} />
+                            ))}
+                        </div>
 
-                        </main>
-                    </div>
+                    </main>
                 </div>
+            </div>
 
 
-            </>
-        );
-    }
-    export default Home
+        </>
+    );
+}
+export default Home
