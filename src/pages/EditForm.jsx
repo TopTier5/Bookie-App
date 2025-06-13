9fb51431305450acdb2e4e0195aa0cf046e80507
vonette-ab <vonwrkspc@gmail.com>
2025-06-13 12:58:05 +0000

    UseNavigate

--- src/pages/EditForm.jsx
+++ src/pages/EditForm.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { useSearchParams } from "react-router";
+import { useSearchParams, useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 import { apiClient } from "../api/client";
 import SubmitButton from "../components/SubmitButton";
